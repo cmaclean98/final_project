df5640037cdf781cd9cca9420f43f0f67af1ef21
cmaclean98 <129405734+cmaclean98@users.noreply.github.com>
2023-05-23 21:22:04 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# final-project
+ 
+![Alt text](http://www.pro-arch.com/uploads/1000010/1540848976897/2018.10.25_Second_Flr_Salon_Public_Space_Page_15-01-1600-xxx_q85.jpg)
---
+++ data/my-points.js
@@ -0,0 +1,181 @@
+const myPoints = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "Icon Parking",
+          "Kind": "Garage",
+          "URL": "https://iconparkingsystems.com/facility/PEARL?utm_source=google&utm_medium=Yext&y_source=1_MjUwOTE2MjAtNzE1LWxvY2F0aW9uLndlYnNpdGU%3D"
+        },
+        "geometry": {
+          "coordinates": [
+            -74.0052097656018,
+            40.707838502958
+          ],
+          "type": "Point"
+        },
+        "id": 1
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "City Parking",
+          "Kind": "Garage",
+          "URL": "https://www.cityparking.nyc/"
+        },
+        "geometry": {
+          "coordinates": [
+            -74.00604593821657,
+            40.70801791815157
+          ],
+          "type": "Point"
+        },
+        "id": 2
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "John Street Parking",
+          "Kind": "Garage",
+          "URL": "https://www.gmcparking.com/locations-rates/downtown/john-street/"
+        },
+        "geometry": {
+          "coordinates": [
+            -74.00648177678069,
+            40.708449633690236
+          ],
+          "type": "Point"
+        },
+        "id": 3
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "GMC Parking",
+          "Kind": "Lot",
+          "URL": "http://www.gmcparking.com/locations-and-rates/downtown/55/"
+        },
+        "geometry": {
+          "coordinates": [
+            -74.00627165477825,
+            40.70857828319487
+          ],
+          "type": "Point"
+        },
+        "id": 4
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "NYC Parking 80 John Garage Corp.",
+          "Kind": "Garage",
+          "URL": "http://nyc.bestparking.com/"
+        },
+        "geometry": {
+          "coordinates": [
+            -74.00691822738163,
+            40.70803695911499
+          ],
+          "type": "Point"
+        },
+        "id": 5
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "201 Pearl Parking Corp",
+          "Kind": "Garage",
+          "URL": "https://ipark.com/garage/goldpearl-parking-corp/"
+        },
+        "geometry": {
+          "coordinates": [
+            -74.00656535768726,
+            40.70703958544229
+          ],
+          "type": "Point"
+        },
+        "id": 6
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "LAZ Parking",
+          "Kind": "Garage",
+          "URL": "http://lazparking.com/"
+        },
+        "geometry": {
+          "coordinates": [
+            -74.0045378882252,
+            40.70708926405888
+          ],
+          "type": "Point"
+        },
+        "id": 7
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "iPark",
+          "Kind": "Garage",
+          "URL": "http://ipark.com/garage/100-william-garage-corp"
+        },
+        "geometry": {
+          "coordinates": [
+            -74.0073532569595,
+            40.70831569469638
+          ],
+          "type": "Point"
+        },
+        "id": 8
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "USA Parking",
+          "Kind": "Lot",
+          "URL": "No Website"
+        },
+        "geometry": {
+          "coordinates": [
+            -74.00558082893386,
+            40.7071918536752
+          ],
+          "type": "Point"
+        },
+        "id": 9
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "240-266 Pearl St Parking",
+          "Kind": "Garage",
+          "URL": "https://www.spotangels.com/#id=2151788&?ref=google_maps_website"
+        },
+        "geometry": {
+          "coordinates": [
+            -74.00466337256324,
+            40.70789763121914
+          ],
+          "type": "Point"
+        },
+        "id": 10
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Name": "Gotham Parking LLC",
+          "Kind": "Lot",
+          "URL": "No Website"
+        },
+        "geometry": {
+          "coordinates": [
+            -74.0071008109457,
+            40.707784896877826
+          ],
+          "type": "Point"
+        },
+        "id": 11
+      }
+    ]
+  }
